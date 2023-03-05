8007b3fc7ffe1b1ce782478d4fe46954057f9f35
Luka Ernestini <luka.ernestini@gmail.com>
2023-03-05 10:25:01 +0100

    add daisyUI

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.css';
+	let title = 'Some Title';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<div class="navbar bg-base-300">
+	<a href="/" class="btn btn-ghost normal-case text-xl">Home</a>
+	<a href="/questions" class="btn btn-ghost normal-case text-xl">Charts</a>
+</div>
+
+<slot />
