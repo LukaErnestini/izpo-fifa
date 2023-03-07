bfa50c0e406e3ff5d3411135d7107ac8d7efc2d1
Luka Ernestini <luka.ernestini@gmail.com>
2023-03-07 10:07:53 +0100

    implement edit of team

--- src/routes/teams/[teamId]/+page.server.ts
+++ src/routes/teams/[teamId]/+page.server.ts
@@ -1,7 +1,25 @@
 import { prisma } from '$lib/server/db';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load = (async ({ params }) => {
-	const team = prisma.team.findFirst({ where: { id: +params.teamId } });
-	return { team };
+	const id = +params.teamId;
+	const team = await prisma.team.findFirst({ where: { id } });
+	return { team, id };
 }) satisfies PageServerLoad;
+
+export const actions = {
+	save: async ({ request }) => {
+		const data = await request.formData();
+		const name = data.get('name') as string;
+		const color = data.get('color') as string;
+		const id = data.get('id') as string;
+		console.log(name, color, id);
+
+		const team = await prisma.team.update({
+			where: { id: +id },
+			data: { color, name }
+		});
+
+		return { team };
+	}
+} satisfies Actions;
--- src/routes/teams/[teamId]/+page.svelte
+++ src/routes/teams/[teamId]/+page.svelte
@@ -1,14 +1,33 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import type { ActionData, PageData } from './$types';
+	import ColorPicker from 'svelte-awesome-color-picker';
 
 	export let data: PageData;
+	export let form: ActionData;
 
-	let name = data.team?.name;
+	let name = form?.team.name || data.team?.name;
+	let hex: string;
+	let id = data.id;
+	if (form?.team.color) {
+		hex = form.team.color;
+	} else if (data.team?.color) {
+		hex = data.team.color;
+	}
 </script>
 
-<input
-	type="text"
-	placeholder="Set team name here"
-	bind:value={name}
-	class="input input-bordered w-full max-w-xs"
-/>
+<form action="?/save" method="post">
+	<input
+		name="name"
+		type="text"
+		placeholder="Set team name here"
+		bind:value={name}
+		class="input input-bordered w-full max-w-xs"
+	/>
+
+	<ColorPicker bind:hex />
+
+	<input type="hidden" name="color" value={hex} />
+	<input type="hidden" name="id" value={id} />
+
+	<button class="btn btn-outline" type="submit">SAVE</button>
+</form>
