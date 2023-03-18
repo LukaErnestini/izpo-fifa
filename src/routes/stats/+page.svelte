b20dd6e84d46784a4c051256e3593a8fef474dca
Luka Ernestini <luka.ernestini@gmail.com>
2023-03-18 16:10:41 +0100

    fix typo

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 
-	export let data: PageData;
+	// export let data: PageData;
 </script>
 
-<p>Comming soon...</p>
+<p>Coming soon...</p>
