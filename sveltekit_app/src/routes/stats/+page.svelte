64333952f8b58e8dd116a61b5cd272bfe0161d04
Luka Ernestini <luka.ernestini@gmail.com>
2023-03-23 18:43:10 +0100

    remove deleted component import

--- sveltekit_app/src/routes/stats/+page.svelte
+++ sveltekit_app/src/routes/stats/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import Histogram from '../../components/stats/Histogram.svelte';
 	import type { PageData } from './$types';
 
 	// export let data: PageData;
 </script>
 
-<Histogram />
+<p>Coming soon...</p>
