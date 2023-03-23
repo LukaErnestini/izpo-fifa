c6f7658738830c7364bfff3f70884a92fe4613b1
Luka Ernestini <luka.ernestini@gmail.com>
2023-03-23 18:37:37 +0100

    comment out example data processing functionality

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+	import Histogram from '../../components/stats/Histogram.svelte';
 	import type { PageData } from './$types';
 
 	// export let data: PageData;
 </script>
 
-<p>Coming soon...</p>
+<Histogram />
