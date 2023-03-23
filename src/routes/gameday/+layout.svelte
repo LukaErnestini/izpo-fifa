bc7915ac23fee75b5aa07ea6328012395ffeb1e5
Luka Ernestini <luka.ernestini@gmail.com>
2023-03-23 03:33:51 +0100

    remove proxy variable

--- src/routes/gameday/+layout.svelte
+++ src/routes/gameday/+layout.svelte
@@ -2,28 +2,26 @@
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
-	let activeGame = data.activeGame;
-
-	$: activeGame = data.activeGame;
+	// $: console.log(`${data.activeGame?.scoreTeamA}:${data.activeGame?.scoreTeamB}`);
 </script>
 
 <div class="navbar bg-base-100">
-	{#if !activeGame}
+	{#if !data.activeGame}
 		<span class="normal-case text-xl">
 			<p>Game Day</p>
 		</span>
 	{:else}
 		<div class="flex justify-between w-full relative">
-			<a href="/settings/teams/{activeGame.teams[0].id}" class="normal-case text-xl"
-				>{activeGame.teams[0].name}</a
+			<a href="/settings/teams/{data.activeGame.teams[0].id}" class="normal-case text-xl"
+				>{data.activeGame.teams[0].name}</a
 			>
 			<div>
-				<span class="normal-case text-2xl">{activeGame.scoreTeamA}</span>
+				<span class="normal-case text-2xl">{data.activeGame.scoreTeamA}</span>
 				<span class="normal-case text-2xl">:</span>
-				<span class="normal-case text-2xl">{activeGame.scoreTeamB}</span>
+				<span class="normal-case text-2xl">{data.activeGame.scoreTeamB}</span>
 			</div>
-			<a href="/settings/teams/{activeGame.teams[1].id}" class="normal-case text-xl"
-				>{activeGame.teams[1].name}</a
+			<a href="/settings/teams/{data.activeGame.teams[1].id}" class="normal-case text-xl"
+				>{data.activeGame.teams[1].name}</a
 			>
 		</div>
 	{/if}
