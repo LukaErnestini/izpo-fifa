3f1bbc5387b5212db9437a4497ddc52df0f79a0e
Luka Ernestini <luka.ernestini@gmail.com>
2023-03-30 17:54:05 +0200

    implement game view

---
+++ sveltekit_app/src/components/game/PrettyTimeline.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import type { Attempt, Player, Team } from '@prisma/client';
+	import PlayerAvatarSelect from './PlayerAvatarSelect.svelte';
+
+	export let attempts: (Attempt & {
+		shooter: Player;
+		assisted: Player | null;
+	})[];
+	export let teams: (Team & {
+		players: Player[];
+	})[];
+
+	// Determine to which team the attempt belongs
+	let attemptsTeam = attempts.map((att) => {
+		if (teams[0].players.find((p) => p.id === att.shooterId)) {
+			return 0;
+		} else {
+			return 1;
+		}
+	});
+</script>
+
+{#each attempts as { time, goal, assisted, shooter }, i}
+	<div>
+		<span id="attempt" class="flex {attemptsTeam[i] ? 'flex-row-reverse' : ''}">
+			<p class="text-sm">{time}'</p>
+			{#if goal}
+				<span> <Icon icon="game-icons:soccer-ball" /></span>
+			{/if}
+
+			<p>
+				{shooter.name}
+			</p>
+
+			{#if assisted}
+				<!-- <PlayerAvatarSelect name={assisted.name} imageUri={assisted.imageUri} width="2rem" /> -->
+				<p class="text-sm">assisted by:</p>
+				<p>{assisted.name}</p>
+			{/if}
+		</span>
+	</div>
+{/each}
+
+<style>
+	#attempt > * {
+		padding-left: 4px;
+		padding-right: 4px;
+	}
+</style>
---
+++ sveltekit_app/src/routes/game/[id]/+page.server.ts
@@ -0,0 +1,47 @@
+import { prisma } from '$lib/server/db';
+import { error, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+
+export const load = (async ({ params }) => {
+	const game = await prisma.game.findFirst({
+		where: { id: +params.id },
+		include: {
+			attempts: { include: { shooter: true, assisted: true } },
+			fouls: true,
+			teams: {
+				include: {
+					players: true
+				}
+			},
+			winner: true
+		}
+	});
+	if (!game) {
+		throw error(404, { message: 'Game not found' });
+	}
+	// Add teamId to each attempt
+	game.attempts = game.attempts.map((attempt) => {
+		// Find the team the shooter belongs to
+		const team = game.teams.find((team) =>
+			team.players.some((player) => player.id === attempt.shooterId)
+		);
+		return {
+			...attempt,
+			teamId: team?.id
+		};
+	});
+	return { game };
+}) satisfies PageServerLoad;
+
+export const actions = {
+	remove: async ({ request }) => {
+		const data = await request.formData();
+		const id = data.get('id') ? +(data.get('id') as string) : undefined;
+		console.log('removing game with id ' + id);
+		const deleteAttempts = prisma.attempt.deleteMany({ where: { gameId: id } });
+		const deleteFouls = prisma.foul.deleteMany({ where: { gameId: id } });
+		const deleteGames = prisma.game.delete({ where: { id } });
+		const transaction = await prisma.$transaction([deleteAttempts, deleteFouls, deleteGames]);
+		throw redirect(301, '/gameday');
+	}
+} satisfies Actions;
