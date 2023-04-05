53a7985875fff4023ddf413e7c0f8b5cd9af3253
Luka Ernestini <luka.ernestini@gmail.com>
2023-04-05 16:12:42 +0200

    fix migration issue, reset database, change schema

--- sveltekit_app/prisma/migrations/20230322121618_add_autogoal/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Attempt" ADD COLUMN     "autogoal" BOOLEAN NOT NULL DEFAULT false;
--- sveltekit_app/prisma/migrations/20230330203946_require_time/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `time` on table `Attempt` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Attempt" ALTER COLUMN "time" SET NOT NULL;
