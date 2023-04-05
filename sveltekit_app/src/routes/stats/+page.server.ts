f6f12dafa3532963772b4a3a030fac8d4064c228
Luka Ernestini <luka.ernestini@gmail.com>
2023-04-05 21:33:44 +0200

    remove unused code

--- sveltekit_app/src/routes/stats/+page.server.ts
+++ sveltekit_app/src/routes/stats/+page.server.ts
@@ -2,9 +2,5 @@ import { prisma } from '$lib/server/db';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
-	// top 3 red cards
-	const redFouls = await prisma.foul.findMany({ where: { card: 'red' } });
-	console.log(redFouls);
-
 	return {};
 }) satisfies PageServerLoad;
