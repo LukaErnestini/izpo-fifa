c29de42cf4619b983344ccc256c54dd496921645
Luka Ernestini <luka.ernestini@gmail.com>
2023-05-09 12:06:46 +0200

    enlarge selected player, colored ring

--- sveltekit_app/src/components/game/PlayerAvatarSelect.svelte
+++ sveltekit_app/src/components/game/PlayerAvatarSelect.svelte
@@ -2,19 +2,23 @@
 	export let name = 'unknown';
 	export let width = '3rem';
 	export let selected = false;
+	export let color = '';
 	export let imageUri: string | null | undefined =
 		'https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg';
+
 	$: if (!imageUri) {
 		imageUri = 'https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg';
 	}
+
+	$: width = selected ? '4rem' : '3rem';
 </script>
 
-<div class="px-1 w-min">
+<div class="px-2 w-min">
 	<div class="avatar block">
 		<div
-			style:width
-			class="rounded-full mx-auto 
-            {selected ? 'ring ring-primary ring-offset-base-100 ring-offset-2' : ''}"
+			style={`width: ${width}; --tw-ring-color: ${color}`}
+			class="rounded-full mx-auto
+			{color ? 'ring ring-offset-1' : ''}"
 		>
 			<img src={imageUri} alt="avatar" />
 		</div>
