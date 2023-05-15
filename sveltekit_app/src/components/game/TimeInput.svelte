54695e028fd52a67049ab42cf9d975c511d22547
Luka Ernestini <luka.ernestini@gmail.com>
2023-05-15 22:03:19 +0200

    update time component style

--- sveltekit_app/src/components/game/TimeInput.svelte
+++ sveltekit_app/src/components/game/TimeInput.svelte
@@ -12,52 +12,39 @@
 </script>
 
 <div class="time-input">
-	<span class="label-text">Time</span>
 	<div class="controls">
-		<button type="button" class="mybutton" on:mousedown={() => changeNum(-5)}>-5</button>
-		<button type="button" class="mybutton" on:mousedown={() => changeNum(-1)}>-</button>
+		<button type="button" class="btn btn-outline w-16" on:mousedown={() => changeNum(-5)}>-5</button
+		>
+		<button type="button" class="btn btn-outline w-12" on:mousedown={() => changeNum(-1)}>-1</button
+		>
 		<span class="time">{time}'</span>
 		<input type="hidden" bind:value={time} name="time" class="" max="130" min="1" />
-		<button type="button" class="mybutton" on:mousedown={() => changeNum(1)}>+</button>
-		<button type="button" class="mybutton" on:mousedown={() => changeNum(5)}>+5</button>
+		<button type="button" class="btn btn-outline w-12" on:mousedown={() => changeNum(1)}>+1</button>
+		<button type="button" class="btn btn-outline w-16" on:mousedown={() => changeNum(5)}>+5</button>
 	</div>
 </div>
 
 <style>
 	.time-input {
 		display: flex;
-		justify-content: space-between;
-		align-items: center;
+		margin: auto;
 	}
 
 	.controls {
 		display: flex;
 		align-items: center;
 		justify-content: center;
+		gap: 8px;
 	}
 
 	.time {
 		font-size: 24px;
-		margin: 0 20px;
+		width: 64px;
+		text-align: center;
 	}
 
-	.mybutton {
-		margin: 4px 4px;
-		border: 2px solid #000;
-		border-radius: 5px;
-		background-color: transparent;
-		padding: 5px 10px;
+	button {
 		font-size: 16px;
-		width: 50px;
-	}
-
-	.mybutton:hover {
-		background-color: #000;
-		color: #fff;
-	}
-
-	.mybutton:focus {
-		outline: none;
-		box-shadow: 0 0 5px #000;
+		font-weight: 500;
 	}
 </style>
