2ec61a12c0170ddaf385fa630ad7776a4d7a1a40
Luka Ernestini <luka.ernestini@gmail.com>
2023-05-15 22:47:23 +0200

    change avatars select style

--- sveltekit_app/src/components/game/SelectPlayersInput.svelte
+++ sveltekit_app/src/components/game/SelectPlayersInput.svelte
@@ -12,21 +12,18 @@
 	}
 </script>
 
-<div class="flex items-center justify-between h-20 px-1">
-	<span class="label-text">{label}</span>
-	<span class="flex">
-		{#each players as { name, imageUri, id }}
-			<label class="cursor-pointer">
-				<PlayerAvatarSelect {imageUri} {name} selected={id === selected ? true : false} />
-				<input
-					type="radio"
-					name={inputName}
-					bind:group={selected}
-					value={id}
-					hidden
-					on:click={() => toggle(id)}
-				/>
-			</label>
-		{/each}
-	</span>
+<div class="flex items-center justify-evenly gap-4 h-20">
+	{#each players as { name, imageUri, id }}
+		<label class="cursor-pointer">
+			<PlayerAvatarSelect {imageUri} {name} selected={id === selected ? true : false} />
+			<input
+				type="radio"
+				name={inputName}
+				bind:group={selected}
+				value={id}
+				hidden
+				on:click={() => toggle(id)}
+			/>
+		</label>
+	{/each}
 </div>
